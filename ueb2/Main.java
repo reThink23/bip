95d2e077738606af38edce4bbe3b785eeb87f6d0
reThink23 <programmingstuffpy@gmail.com>
2023-10-19 23:51:57 +0200

    remove unused imports

--- ueb2/Main.java
+++ ueb2/Main.java
@@ -1,10 +1,7 @@
 package ueb2;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
