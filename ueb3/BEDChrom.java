78329b49f1a564c773a67b0c291fda03e83dad9a
reThink23 <42834529+reThink23@users.noreply.github.com>
2023-11-06 09:52:02 +0100

    recordify bedchrom

--- ueb3/BEDChrom.java
+++ ueb3/BEDChrom.java
@@ -2,75 +2,39 @@ package ueb3;
 
 import java.util.Comparator;
 
-public class BEDChrom implements Comparator<BEDChrom>, Comparable<BEDChrom> {
+public record BEDChrom (String chrom, Integer chromStart, Integer chromEnd, String name, Integer score, Strand strand) implements Comparator<BEDChrom>, Comparable<BEDChrom> {
 
-	private String chrom;
-	private Integer chromStart;
-	private Integer chromEnd;
-	private String name;
-	private Integer score;
-	private Strand strand;
-
-	public BEDChrom() {}
-
-	public BEDChrom(String chrom, Integer chromEnd, Integer chromStart, String name, Integer score, Strand strand) { 
-		setAll(chrom, chromEnd, chromStart, name, score, strand);
-	}
-	public BEDChrom(String chrom, String chromEnd, String chromStart, String name, String score, String strand) {
-		if (score.equals(".")) score = "0";
-		setAll(chrom, Integer.valueOf(chromStart), Integer.valueOf(chromEnd), name, score.equals(".") ? null : Integer.valueOf(score), symbolToStrand(strand));
-	}
-
-	
-	public void setChrom(String chrom) { this.chrom = chrom; }
-	public void setName(String name) {this.name = name;}
-	public void setStrand(Strand strand) {this.strand = strand;}
-	
-	public void setChromStart(Integer chromStart) {
-		if (this.chromEnd != null) if (this.chromEnd < chromStart ) 
-		throw new IllegalArgumentException(String.format("chromStart (%d) has to be lower than chromStart (%d)", chromStart, chromEnd));
-		this.chromStart = chromStart;
-	}
-	public void setChromEnd(Integer chromEnd) {
-		if (this.chromStart != null) if (this.chromStart > chromEnd ) 
-		throw new IllegalArgumentException(String.format("chromEnd (%d) has to be higher than chromStart (%d)", chromEnd, chromStart));
-		this.chromEnd = chromEnd;
-	}
-	
-	public void setScore(Integer score) {
-		if (score == null) this.score = null;
+	public BEDChrom(String chrom, Integer chromStart, Integer chromEnd, String name, Integer score, Strand strand) {
+		if (chrom == null || chromStart == null || chromEnd == null || name == null || strand == null) 
+			throw new IllegalArgumentException("Chrom, chromStart, chromEnd, name and strand have to be set");
 		if (score < 0 || score > 1000 ) throw new IllegalArgumentException(score+" is not a score. The score should be between 0 and 1000");
+		if (chromEnd <= chromStart ) 
+			throw new IllegalArgumentException(String.format("chromStart (%d) has to be lower than chromStart (%d)", chromStart, chromEnd));
+		
+		this.chrom = chrom;
+		this.chromStart = chromStart;
+		this.chromEnd = chromEnd;
+		this.name = name;
 		this.score = score;
-	}
+		this.strand = strand;
 
-	public void setAll(String chrom, Integer chromEnd, Integer chromStart, String name, Integer score, Strand strand) {
-		setChrom(chrom);
-		setChromStart(chromStart);
-		setChromEnd(chromEnd);
-		setName(name);
-		setScore(score);
-		setStrand(strand);
 	}
-
-	public String getChrom() {return chrom;}
-	public int getChromStart() {return chromStart;}
-	public int getChromEnd() {return chromEnd;}
-	public String getName() {return name;}
-	public int getScore() {return score;}
-	public Strand getStrand() {return strand;}
+	public BEDChrom(String chrom, String chromStart, String chromEnd, String name, String score, String strand) {
+		this(chrom, Integer.valueOf(chromStart), Integer.valueOf(chromEnd), name, score.equals(".") ? null : Integer.valueOf(score), symbolToStrand(strand));
+	}
 
 	public enum Strand {
 		POSITIVE("+"), NEGATIVE("-"), NONE(".");
 		
 		private final String symbol;
-		
+
 		Strand(String symbol) { this.symbol = symbol; }
+
 		public static Strand toStrand(String symbol) { return symbolToStrand(symbol);}
-		public String getSymbol() { return symbol;}
 		public String toString() { return (this == NONE) ? "." : (this == POSITIVE) ? "+" : "-";}
 	}
 
-	public static Strand symbolToStrand(String symbol) {
+	private static Strand symbolToStrand(String symbol) {
 		switch (symbol) {
 			case "+":
 				return Strand.POSITIVE;
